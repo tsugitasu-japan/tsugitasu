92cbde9d9eb8fc590c21adfd4026392a2a375f1c
SotaKojima <sotakojima0525@gmail.com>
2022-06-22 17:25:52 +0900

    ダッシュボード統合

---
+++ src/views/Dashboard.vue
@@ -0,0 +1,29 @@
+<template>
+<div class="dashboard">
+  <HeaderLogin></HeaderLogin>
+  <div class="content">
+    <DashboardSidebar></DashboardSidebar>
+    <DashboardClass></DashboardClass>
+  </div>
+</div>
+</template>
+
+<script>
+  import HeaderLogin from '../components/HeaderLogin.vue'
+  import DashboardSidebar from '../components/DashboardSidebar.vue'
+  import DashboardClass from '../components/DashboardClass.vue'
+
+  export default{
+    components:{
+      HeaderLogin,
+      DashboardSidebar,
+      DashboardClass,
+    }
+  }
+</script>
+
+<style scoped>
+.content{
+  display: flex;
+}
+</style>
