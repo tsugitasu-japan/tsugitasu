fcdf9a6bb75f303cc964ff34531a4f75f470a1af
tone8151 <68996392+tone8151@users.noreply.github.com>
2022-07-17 17:33:44 +0900

    ブランチのテスト (#1)
    Co-authored-by: Yukihiko Tone <yukihikotone@YukihikonoMacBook-Pro.local>

--- src/views/Index.vue
+++ src/views/Index.vue
@@ -1,7 +1,7 @@
 <template>
 <div>
   <Header></Header> 
-  <router-link to="/dashboard/class">ダッシュボード画面へ</router-link>
+  <router-link to="/dashboard/class">move to ダッシュボード</router-link>
   <!-- <router-link to="/test">ラムダテストページへ</router-link>
   <router-link to="/cropper">Cropperテストページへ</router-link> -->
 </div>
