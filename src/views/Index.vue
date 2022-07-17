6766f62b03b8927621c3d76547b2b4e0f50f88dd
Yukihiko Tone <yukihikotone@YukihikonoMacBook-Pro.local>
2022-07-17 17:39:01 +0900

    コミットテスト

--- src/views/Index.vue
+++ src/views/Index.vue
@@ -1,7 +1,7 @@
 <template>
 <div>
   <Header></Header> 
-  <router-link to="/dashboard/class">move to ダッシュボード</router-link>
+  <router-link to="/dashboard/class">ダッシュボードへ遷移</router-link>
   <!-- <router-link to="/test">ラムダテストページへ</router-link>
   <router-link to="/cropper">Cropperテストページへ</router-link> -->
 </div>
